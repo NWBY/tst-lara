466ac86d8e3a06cf1224051a838521272be81144
Sam Newby <sam.newby19@gmail.com>
2025-03-31 17:35:19 +0100

    chore: fix

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (Request $request) {
-    logger()->info('Request', [
-        'ip' => $request->ip(),
-        'headers' => $request->headers,
-    ]);
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/ip', function (Request $request) {
     return response()->json([
         'ip' => $request->ip(),
         'ips' => $request->ips(),
         'headers' => request()->headers->all(),
-        'server' => $_SERVER,
     ]);
 });
