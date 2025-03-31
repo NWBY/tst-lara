159f0398dc5b6e02542a2a43c9af4d75670df705
Sam Newby <sam.newby19@gmail.com>
2025-03-31 16:13:19 +0100

    chore: fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    return response()->json([
+        'ip' => $request->ip(),
+        'x-forwarded-for' => $request->header('x-forwarded-for'),
+    ]);
 });
