0e7d94764f3553aa3c1051fec2f74653dd99c32a
Josh Bonfield <josh@grizzlyware.com>
2025-04-01 13:50:14 +0100

    chore trust local

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies([
+            '10.0.0.0/8',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
